745f61f27a5668c73207c900e7f6bdaea79e9dca
salafimanhaj <67724995+salafimanhaj@users.noreply.github.com>
2024-07-14 06:27:53 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -46,7 +46,7 @@ const ar = defineCollection({
       featured: z.boolean().optional().default(false),
       trans: z.string().optional(),
       draft: z.boolean().optional(),
-      tags: z.array(z.string()).optional().default(["fatwas"]),
+      tags: z.array(z.string()).optional().default(["فتاوى"]),
       ogImage: image()
         .refine(img => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
